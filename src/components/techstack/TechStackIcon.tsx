e39f17767029246c2434dfd5dc40fd6d6579589b
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-07-12 15:39:10 +0800

    initial Test TechStack

--- src/components/techstack/TechStackIcon.tsx
+++ src/components/techstack/TechStackIcon.tsx
@@ -14,8 +14,8 @@ const TechStackIcon = ({
     <div className="relative flex">
       <Hexagon fillColor="#001C55" width="115" height="120" />
       <div className="absolute top-6 right-[18%]">
-        <SvgIcon height="50" viewBox="0 0 70 80" />
-        <p className="mt-1 text-secondary text-center font-semibold text-lg">
+        <SvgIcon height="50" viewBox="0 0 70 85" />
+        <p className="mt-1 text-secondary text-center font-semibold text-md">
           {techStackName}
         </p>
       </div>
--- src/components/techstack/TechStackPage.tsx
+++ src/components/techstack/TechStackPage.tsx
@@ -1,26 +1,60 @@
 "use client";
-import dynamic from "next/dynamic";
 
 import TechStackIcon from "./TechStackIcon";
-
-const HtmlIcon = dynamic(() => import("../../../public/techstack/HTML.svg"), {
-  ssr: false,
-});
-const CSSIcon = dynamic(() => import("../../../public/techstack/CSS.svg"), {
-  ssr: false,
-});
-const ReactIcon = dynamic(() => import("../../../public/techstack/React.svg"), {
-  ssr: false,
-});
+import {
+  HtmlIcon,
+  CSSIcon,
+  JSIcon,
+  TSIcon,
+  ReactIcon,
+  TailwindIcon,
+  CodeIcon,
+  GitIcon,
+  NextIcon,
+  FigmaIcon,
+} from "./TechStackImport";
 
 const TechStackPage = () => {
   return (
     <section className="bg-accent pt-10">
-      <div className="flex flex-col gap-2 max-w-[80%] mx-auto">
-        <div className="flex items-center">
-          <TechStackIcon svgIcon={HtmlIcon} techStackName="HTML" />
-          <TechStackIcon svgIcon={CSSIcon} techStackName="CSS" />
-          <TechStackIcon svgIcon={ReactIcon} techStackName="React" />
+      <div className="grid grid-cols-1 md:grid-cols-2 max-w-[80%] mx-auto">
+        <div className="relative flex flex-col flex-wrap">
+          {/* First Row */}
+          <div className="flex items-center">
+            <TechStackIcon svgIcon={HtmlIcon} techStackName="HTML" />
+            <TechStackIcon svgIcon={CSSIcon} techStackName="CSS" />
+            <TechStackIcon svgIcon={JSIcon} techStackName="JavaScript" />
+            <TechStackIcon svgIcon={ReactIcon} techStackName="React" />
+            <TechStackIcon svgIcon={CodeIcon} techStackName="VSCode" />
+          </div>
+
+          {/* Second Row */}
+          <div className="ml-10 flex items-center">
+            <TechStackIcon svgIcon={TailwindIcon} techStackName="Tailwind" />
+            <TechStackIcon svgIcon={TSIcon} techStackName="TypeScript" />
+            <TechStackIcon svgIcon={NextIcon} techStackName="Next JS" />
+            <TechStackIcon svgIcon={GitIcon} techStackName="Git" />
+            <TechStackIcon svgIcon={FigmaIcon} techStackName="Figma" />
+          </div>
+        </div>
+
+        {/* Card Section */}
+        <div className="">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            voluptates adipisci commodi impedit animi iste, illum veritatis
+            necessitatibus repellat deserunt molestias, error laborum
+            distinctio, nihil delectus. Exercitationem dolorem obcaecati
+            necessitatibus beatae aspernatur reprehenderit, nisi natus
+            consequatur rem repellat asperiores est! Molestias maxime provident
+            corporis illum iure totam aspernatur temporibus recusandae sint hic
+            voluptate animi similique, consequatur veniam quisquam commodi non,
+            mollitia eos enim id laborum. Corrupti esse unde quis nam dolorem
+            mollitia voluptatum omnis architecto eos officia aut ut possimus
+            vero obcaecati ipsum voluptatibus, quos iste doloribus, consequuntur
+            nihil ipsa! Quas doloremque molestiae nostrum fugiat sit animi
+            voluptates ratione quod!
+          </p>
         </div>
       </div>
     </section>
