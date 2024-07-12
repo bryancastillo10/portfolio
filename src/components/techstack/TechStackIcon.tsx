d4d5f37dff7ebd8b6e0e7db25fa342f71f3081d4
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-07-12 10:08:14 +0800

    temporary fix

--- src/components/techstack/TechStackIcon.tsx
+++ src/components/techstack/TechStackIcon.tsx
@@ -1,32 +1,22 @@
 import Hexagon from "../ui/Hexagon";
-import Image,{StaticImageData} from "next/image";
-import { SVGProps } from "react";
+import Image, { StaticImageData } from "next/image";
 
-interface TechStackIconProps{
-    svgIcon: React.FC<SVGProps<SVGSVGElement>>;
-    techStackName: string;
+interface TechStackIconProps {
+  svgIcon: StaticImageData;
+  techStackName: string;
 }
 
-const TechStackIcon = ({svgIcon:SvgIcon,techStackName}:TechStackIconProps) => {
+const TechStackIcon = ({ svgIcon, techStackName }: TechStackIconProps) => {
   return (
     <div className="relative">
-          <Hexagon
-              fillColor="#001C55"
-              width="200"
-              height="180"
-          />
-          <div className="absolute top-10 left-14 flex flex-col gap-1 items-center">
-              <SvgIcon width="80" height="80" /> 
-              {/* <Image
-                  src={svgIcon}
-                  height="70"
-                  width="70"
-                  alt="tech-icon"
-              /> */}
-              <p className="text-secondary font-semibold text-lg">{techStackName}</p>
-        </div>
+      <Hexagon fillColor="#001C55" width="200" height="180" />
+      <div className="absolute top-10 left-14 flex flex-col gap-1 items-center">
+        {/* <SvgIcon width="80" height="80" />  */}
+        <Image src={svgIcon} height="70" width="70" alt="tech-icon" />
+        <p className="text-secondary font-semibold text-lg">{techStackName}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default TechStackIcon;
