b6991a1cfbf7f17fe9e2c3fdb2949a9a14316600
bryancastillo10 <bryan.angelocastillo.10@gmail.com>
2024-07-13 08:58:47 +0800

    Tech Stack layout revise and new array splitting

---
+++ src/utils/arraySplit.ts
@@ -0,0 +1,9 @@
+function arraySplit (array:any, size:number) {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+        result.push(array.slice(i, i + size));
+    }
+    return result;
+};
+
+export default arraySplit;
