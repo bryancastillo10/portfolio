c80d623d938e6391492a3ea67de37a70298ca73d
bryancastillo10 <bryan.angelocastillo.10@gmail.com>
2024-07-14 11:39:24 +0800

    Tech Cards established

---
+++ src/components/techstack/TechArticle.tsx
@@ -0,0 +1,13 @@
+import Headers from "../ui/Headers";
+const TechArticle = () => {
+  return (
+      <section className="space-y-4">
+          <Headers>Right Tools for the Job</Headers>
+          <p className="text-md">
+            Software development
+          </p>
+    </section>
+  )
+}
+
+export default TechArticle;
