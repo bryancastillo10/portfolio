1149f2e987feecc048cfedf9ecedd72c8c8860d6
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-08-18 21:07:19 +0800

    modularize hero section

---
+++ src/app/(components)/Hero/TopProjects.tsx
@@ -0,0 +1,14 @@
+
+
+const TopProjects = () => {
+  return (
+    <div className="mt-8 max-w-[80%] mx-auto space-y-10">
+    <h1 className="text-2xl">Project 1: ChemStack</h1>
+    <h1 className="text-2xl">Project 2: SpaceChat</h1>
+    <h1 className="text-2xl">Project 3: NPK Calculator</h1>
+    <h1 className="text-2xl">Project 4: Biobytes Pairsync</h1>
+</div>
+  )
+}
+
+export default TopProjects;
