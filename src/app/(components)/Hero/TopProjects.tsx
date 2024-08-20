a8a63cad3970bc3821818992448feb906f264544
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-08-20 10:53:58 +0800

    ProjectCardsCarousel

--- src/app/(components)/Hero/TopProjects.tsx
+++ src/app/(components)/Hero/TopProjects.tsx
@@ -1,43 +1,100 @@
 "use client";
-import Image from "next/image";
-import { PlayCircle, GitForkIcon, ChevronLeft, ChevronRight } from "lucide-react";
-import { projectInfo } from "./projectInfo"; 
-import useCardCarousel from "./useCardCarousel";
+import { PlayCircle, GitForkIcon } from "lucide-react";
+import { useState, useEffect } from "react";
+import { useMotionValue, motion } from 'framer-motion';
+import { TopProjectsProps, 
+          Auto_Delay, 
+          Drag_Buffer, 
+          Spring_Config,
+          IndicatorProps, 
+           } from './useCardCarousel';
 
-const TopProjects = () => {
-  const { slideIndex, handlePrevious, handleNext } = useCardCarousel(projectInfo);
+
+
+export const TopProjects = ({projectInfo}:TopProjectsProps) => {
+  const [cardIndex, setCardIndex] = useState(0);
+
+  const dragX = useMotionValue(0);
+
+  useEffect(() => {
+    const intervalRef = setInterval(() => {
+      const x = dragX.get();
+
+      if (x === 0) {
+        setCardIndex((pv) => {
+          if (pv === projectInfo.length - 1) {
+            return 0;
+          }
+          return pv + 1;
+        });
+      }
+    }, Auto_Delay);
+
+    return () => clearInterval(intervalRef);
+  }, []);
+
+  const onDragEnd = () => {
+    const x = dragX.get();
+
+    if (x <= -Drag_Buffer && cardIndex < projectInfo.length - 1) {
+      setCardIndex((pv) => pv + 1);
+    } else if (x >= Drag_Buffer && cardIndex > 0) {
+      setCardIndex((pv) => pv - 1);
+    }
+  };
   return (
-    <section className="mt-8 max-w-[80%] mx-auto space-y-10">
-      <h1 className="pt-10 text-4xl tracking-wider">Top Projects</h1>
-      <div className="p-12 flex flex-col justify-center items-center gap-4 mb-12 md:mb-6">
-        <div className="flex relative transition-transform ease-out duration-1000">
-          {/* Card Container */}
-          {projectInfo.map((project, i) => (
-            <div
-              key={project.id}
-              className={`p-6 flex justify-between shrink-0 gap-2 mx-auto shadow-md w-[300px] h-[430px] md:w-[700px] md:h-[300px] relative rounded-2xl transition-transform ${
-                i === slideIndex ? "block" : "hidden"
-              }`}
-            >
-              {/* Project Image */}
-              <div className="absolute inset-0 z-0">
-                <Image
-                  src={project.image || "https://placehold.co/550x300"}
-                  layout="fill"
-                  objectFit="cover"
-                  className="object-cover rounded-2xl "
-                  unoptimized
-                  alt={`${project.projectTitle}-img`}
-                />
-                {/* Teal Overlay */}
-                <div className="absolute inset-0 bg-teal-500/50" />
-              </div>
-              {/* Header */}
-              <div className="relative z-10 space-y-2">
-                <h1 className="text-2xl font-semibold tracking-wide">{project.projectTitle}</h1>
-                <h3 className="text-lg">{project.projectSubtitle}</h3>
+    <div className="relative overflow-hidden pb-8">
+      <motion.div
+        drag="x"
+        dragConstraints={{
+          left: 0,
+          right: 0,
+        }}
+        style={{
+          x: dragX,
+        }}
+        animate={{
+          translateX: `-${cardIndex * 100}%`,
+        }}
+        transition={Spring_Config}
+        onDragEnd={onDragEnd}
+        className="flex cursor-grab items-center active:cursor-grabbing"
+      >
+        <Cards projectInfo={projectInfo} />
+      </motion.div>
+
+      <Indicator projectInfo={projectInfo} cardIndex={cardIndex} setCardIndex={setCardIndex} />
+
+    </div>
+  );
+};
+
+
+const Cards = ({projectInfo}:TopProjectsProps) => {
+  return (
+    <>
+      {projectInfo.map((project,idx)=> {
+        return (
+          <motion.div
+            key={project.id}
+            style={{
+              backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${project.image})`,
+              backgroundSize:"cover",
+              backgroundPosition:"center"
+            }}
+
+            animate={{
+              scale: project.id === idx ? 0.95 : 0.85,
+            }}
+
+            transition={Spring_Config}
+            className="w-full h-[71vh] shrink-0 rounded-xl object-cover"
+          >
+            <div className="p-6 md:p-10 space-y-2 text-white">
+                <h1 className="text-4xl md:text-7xl font-semibold tracking-wider">{project.projectTitle}</h1>
+                <h3 className="py-4 text-3xl">{project.projectSubtitle}</h3>
                 {/* Description */}
-                <p className="pt-4 text-sm text-balance">{project.description}</p>
+                <p className="pt-4 text-xl text-balance">{project.description}</p>
 
                 {/* Tech Stack */}
                 <div className="pt-4">
@@ -45,7 +102,7 @@ const TopProjects = () => {
                     {project.techStack.map((tech, index) => (
                       <li
                         key={index}
-                        className="max-w-fit font-light shadow-lg bg-secondary text-primary px-4 py-2 rounded-2xl"
+                        className="md:text-2xl max-w-fit font-light shadow-lg bg-secondary text-primary px-4 py-2 rounded-2xl"
                       >
                         {tech}
                       </li>
@@ -54,60 +111,45 @@ const TopProjects = () => {
                 </div>
 
                 {/* Links */}
-                <div className="pt-6 md:pt-4 grid grid-cols-2 md:justify-evenly mx-2 md:max-w-[50%] ">
-                  <div className="flex flex-col gap-1">
-                    <a href={project.demoLink} target="_blank" rel="noopener noreferrer">
-                      <PlayCircle size={28} />
+                <div className="pt-8 md:pt-12 flex  justify-start gap-10  mx-2 md:max-w-[50%]">
+                  <div className="flex flex-col items-center gap-2 duration-500 ease-out p-4 ">
+                    <a href={project.demoLink} target="_blank" rel="noopener noreferrer ">
+                      <PlayCircle size={50} className="" />
                     </a>
-                    <small>Demo</small>
+                    <p className="text-2xl">Demo</p>
                   </div>
-                  <div className="flex flex-col items-center gap-1">
+                  <div className="flex flex-col items-center gap-2 duration-500 ease-out p-4">
                     <a href={project.gitRepo} target="_blank" rel="noopener noreferrer">
-                      <GitForkIcon size={28} />
+                      <GitForkIcon size={50} />
                     </a>
-                    <small>Repository</small>
+                    <p className="text-2xl">Repository</p>
                   </div>
                 </div>
-              </div>
-            </div>
-          ))}
+             </div>         
+          </motion.div>
+        )
+      })}
+    </>
+  )
+}
 
-          {/* Carousel Arrows */}
-          <div
-            className="flex items-center justify-between left-[-80px] right-[-80px]
-            md:right-[-60px] md:left-[-60px]  p-4 absolute z-0 inset-0"
-          >
-            <button
-              onClick={handlePrevious}
-              className="p-1 rounded-full text-black shadow-md bg-white/50 hover:bg-white"
-            >
-              <ChevronLeft size={40} />
-            </button>
-            <button
-              onClick={handleNext}
-              className="p-1 rounded-full text-black shadow-md bg-white/50 hover:bg-white"
-            >
-              <ChevronRight size={40} />
-            </button>
-          </div>
-
-          {/* Carousel Indicator */}
-          <div className="absolute bottom-0 md:bottom-0 left-0 right-0">
-            <div className="flex justify-center items-center gap-2">
-              {projectInfo.map((_, i) => (
-                <div
-                  key={i}
-                  className={`transition-all size-3 bg-white rounded-full ${
-                    slideIndex === i ? "p-2" : "bg-opacity-50"
-                  }`}
-                />
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+const Indicator = ({ projectInfo, cardIndex, setCardIndex }:IndicatorProps) => {
+  return (
+    <div className="mt-4 flex w-full justify-center gap-2">
+      {projectInfo.map((_, idx) => {
+        return (
+          <button
+            key={idx}
+            onClick={() => setCardIndex(idx)}
+            className={`h-3 w-3 rounded-full transition-colors ${
+              idx === cardIndex ? "bg-light-secondary" : "bg-neutral-500"
+            }`}
+          />
+        )
+      })}
+    </div>
   );
 };
 
-export default TopProjects;
+
+
