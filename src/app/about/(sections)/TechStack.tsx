d41ef9fa4d19afe160edbbd82803ecf6cdd3fae5
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-08-20 21:32:50 +0800

    temporary comment array

--- src/app/about/(sections)/TechStack.tsx
+++ src/app/about/(sections)/TechStack.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { adventPro } from "@/utils/font";
-import techStacks from "../techStack";
+// import techStacks from "../techStack";
 
 const TechStack = () => {
     
@@ -9,14 +9,14 @@ const TechStack = () => {
     <div className="py-4 w-full h-[600px] md:h-[300px] border">
     <h1 className={`text-left mt-4 text-5xl ${adventPro.className}`}>My Tools for Development</h1>
     <div>
-      {techStacks.map((stack) => (
+      {/* {techStacks.map((stack) => (
         <div key={stack.id}>
           <stack.iconComponent fill={stack.fillColor} width={50} height={50} />
           <h3>{stack.name}</h3>
           <p>{stack.category}</p>
           <p>{stack.description}</p>
         </div>
-      ))}
+      ))} */}
     </div>
     
     </div>
