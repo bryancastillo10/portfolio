69243fcae2f18aa21b28d7511eefbf45f244268a
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-08-23 07:15:25 +0800

    Modal initial style

--- src/app/(components)/Modal/index.tsx
+++ src/app/(components)/Modal/index.tsx
@@ -17,19 +17,19 @@ const Modal = ({ openModal, title, onClose, body }: ModalProps) => {
     };
 
     return (
-        <div className={`fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none bg-neutral-800/70 transition-opacity duration-500 ${openModal ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-            <div className="relative w-full h-full md:w-[80%] lg:w-[50%] xl:w-[40%] mx-auto md:h-auto">
+        <div className={`fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none bg-slate-500/20 backdrop-blur-[1px] transition-opacity duration-500 ${openModal ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
+            <div className="relative w-full h-full md:w-[80%] lg:w-[50%] mx-auto md:h-auto">
                 {/* Modal Content */}
                 <div className={`transform transition-transform duration-500 ${openModal ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"}`}>
-                    <div className="relative w-full h-full md:h-auto border-0 rounded-lg shadow-lg flex flex-col bg-white outline-none focus:outline-none">
+                    <div className="relative w-full h-full md:h-auto border-0 rounded-xl shadow-lg flex flex-col bg-glassmorphism outline-none focus:outline-none">
                         {/* Header */}
-                        <div className="relative flex items-center p-6 justify-center rounded-t border-b">
+                        <div className="relative flex p-6 justify-center rounded-t">
                             <button onClick={handleCloseModal}
                                 className="p-1 border-0 hover:opacity-70 transition absolute right-9 hover:text-rose-500">
                                 <XIcon size={18} />
                             </button>
-                            <div className="text-lg font-semibold">
-                                {title}
+                            <div className="text-2xl font-semibold">
+                                Project Name: {title}
                             </div>
                         </div>
                         {/* Body */}
@@ -37,8 +37,8 @@ const Modal = ({ openModal, title, onClose, body }: ModalProps) => {
                             {body}
                         </div>
                         {/* Footer */}
-                        <div className="p-6 flex flex-col gap-2">
-                            <Button action={handleCloseModal} variant="primary">Got It</Button>
+                        <div className="p-6 flex flex-col gap-2 items-center">
+                            <Button action={handleCloseModal} variant="secondary">Got It</Button>
                         </div>
                     </div>
                 </div>
