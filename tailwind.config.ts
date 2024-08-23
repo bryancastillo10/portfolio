debcfbabdbce406897b78fec1fd0efc566bac411
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-08-23 14:59:24 +0800

    MarqueeAnimation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,6 +29,18 @@ const config: Config = {
       },
       animation: {
         "spin-slow": "spin 8s linear infinite",
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateY(0%)' },
+          '100%': { transform: 'translateY(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0%)' },
+        },
       },
     },
   },
