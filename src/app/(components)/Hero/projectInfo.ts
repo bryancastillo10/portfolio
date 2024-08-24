a971b6378b5085c96422fbe7d25360abc6a6aff2
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-08-24 15:52:58 +0800

    topprojects migrated

--- src/app/(components)/Hero/projectInfo.ts
+++ src/app/(components)/Hero/projectInfo.ts
@@ -1,8 +1,4 @@
-import ChemStashImg from "@/asset/topprojects/ChemStash.png";
-import SpaceChatImg from "@/asset/topprojects/SpaceChat.png";
-import NPKCalculatorImg from "@/asset/topprojects/NPKCalculator.png";
-import BioBytesImg from "@/asset/topprojects/BioBytes.png";
-import AirBnbCloneImg from "@/asset/topprojects/AirBnbClone.png";
+
 
 export const projectInfo = [
     {
@@ -11,7 +7,7 @@ export const projectInfo = [
         projectSubtitle:"Chemical Inventory Management App",
         description:"A collaboration project which can help in managing the logistics of chemicals at a chemical laboratory. This provides convenient insights to chemistry professionals on the traceability, consumption, and safety on chemicals",
         techStack:["MERN Stack", "Recharts","Socket IO", "Tailwind CSS"],
-        image: ChemStashImg.src,
+        image: "/topprojects/ChemStash.png",
         demoLink:"https://mern-with-auth-rba5.onrender.com/",
         gitRepo:""
     },
@@ -21,7 +17,7 @@ export const projectInfo = [
         projectSubtitle:"Real Time Messaging App",
         description:"This small chat application can help you connect with friends with simple steps to create your account. Also have features that is common to a social media platform.",
         techStack:["MERN Stack","Typescript", "Socket IO", "Tailwind CSS"],
-        image: SpaceChatImg.src,
+        image: "/topprojects/SpaceChat.png",
         demoLink:"https://space-chat-ywt3.onrender.com",
         gitRepo:"https://github.com/bryancastillo10/space-chat"
     },
@@ -31,7 +27,7 @@ export const projectInfo = [
         projectSubtitle:"Fertilizer Composition Calculator Based on Raw Materials Database",
         description:"Prototype build to calculate and suggest appropriate nutrient composition based on a database query. This is helpful to provide suggestions on fertilizer composition based on a database about different raw materials",
         techStack:["Python-Flask","React JS","Typescript","Tailwind CSS","PostgreSQL"],
-        image: NPKCalculatorImg.src,
+        image: "topprojects/NPKCalculator.png",
         demoLink:"https://npk-calculator.onrender.com/",
         gitRepo:""
     },
@@ -41,7 +37,7 @@ export const projectInfo = [
         projectSubtitle:"Obtaining Fast Insights from Biological Data by providing input sequences (DNA, RNA, or Protein)",
         description:"Leveraging the PyQt library of Python to develop a desktop application for biologists to gain information on several biological sequences such as GC-content, base frequencies and basic pairwise sequencing",
         techStack:["PyQt5","Matplotlib","Numpy"],
-        image: BioBytesImg.src,
+        image: "topprojects/Biobytes.png",
         demoLink:"https://sourceforge.net/topprojects/biobytes-pairsync/",
         gitRepo:"https://github.com/bryancastillo10/BioBytes-PairSync"
     },
@@ -51,7 +47,7 @@ export const projectInfo = [
         projectSubtitle:"Clone of Famous Accomodation Booking App",
         description:"Imitating the key features of the popular accommodation reservation app that further provides me insights on how to work on databases. This app has authentication, user reservation, user likings, and ability to add their own places as an accomodation",
         techStack:["Next JS","MongoDB","Prisma", "Tailwind CSS"],
-        image:AirBnbCloneImg.src,
+        image:"/topprojects/AirBnbClone.png",
         demoLink:"https://airbnb-testclone.vercel.app/",
         gitRepo:"https://github.com/bryancastillo10/airbnb-clone"
     }
