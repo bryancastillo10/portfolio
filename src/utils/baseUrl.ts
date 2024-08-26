c988a0e2705a301f1667ba2e8bddd3e8546f9f7e
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-08-26 17:59:31 +0800

    update base URL

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,5 +1,14 @@
-export const baseUrl = typeof window === 'undefined'
-? process.env.VERCEL_URL // For production
-? `https://${process.env.VERCEL_URL}`
-: 'http://localhost:3000' // For development
-: ''; 
+export const baseUrl = (() => {
+    if (typeof window === 'undefined') {
+      // Server-side
+      if (process.env.VERCEL_URL) {
+        return `https://${process.env.VERCEL_URL}`;
+      }
+      return process.env.NODE_ENV === 'production' 
+        ? 'https://bryan-castillo-portfolio.vercel.app/'  
+        : 'http://localhost:3000';
+    } else {
+      // Client-side
+      return '';
+    }
+  })();
