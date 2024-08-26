8260019930f74cd796406b659d27017bff5adc43
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-08-26 15:19:45 +0800

    Hero Alignment

--- src/app/(components)/Hero/index.tsx
+++ src/app/(components)/Hero/index.tsx
@@ -33,14 +33,14 @@ const Hero = () => {
        <section style={backgroundStyle}>
           <div className="max-w-[80%] mx-auto container pt-20">
             {/* Hero Header */}
-            <div className={`my-4 md:p-4 flex flex-col gap-4 text-center md:text-left  rounded-2xl max-w-[80%] xl:max-w-[50%] mx-auto
+            <div className={`my-4 md:p-4 flex flex-col gap-4 text-center md:text-left rounded-2xl max-w-[80%] xl:max-w-[50%] mx-auto md:mx-0
               ${theme ? "bg-secondary":"bg-primary/80"}`}>
               <RevealText isLeft>
               <h1 className={`mt-8 md:mt-4 font-bold text-7xl tracking-wide ${adventPro.className}`}>
                 Bryan Castillo
               </h1>
               <div className="min-w-fit md:max-w-full py-4 ">
-                <p className="md:font-medium leading-sm text-left text-xl md:text-2xl max-w-[90%] mx-auto md:mx-0">
+                <p className="md:font-medium leading-sm text-center md:text-left text-xl md:text-2xl max-w-[90%] mx-auto md:mx-0">
                   Crafting creative and innovative solutions from &nbsp;
                   <span className="font-bold">science</span> to &nbsp;
                   <span className="font-bold">software development</span>
