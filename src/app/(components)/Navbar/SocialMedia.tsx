1f2c0405f4acd24c8f3a24446e2aac7e58d9a680
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2024-08-26 15:53:39 +0800

    SocialMediaLink

--- src/app/(components)/Navbar/SocialMedia.tsx
+++ src/app/(components)/Navbar/SocialMedia.tsx
@@ -4,10 +4,18 @@ const SocialMedia = () => {
 
   return (
     <div className="flex gap-3">
-      <Facebook/>
-      <Instagram/>
-      <Github/>
-      <Linkedin/>
+      <a href="https://www.facebook.com/scientistbryan" target="_blank" rel="noopener noreferrer">
+        <Facebook/>
+      </a>
+      <a href="https://www.instagram.com/brybryanium/" target="_blank" rel="noopener noreferrer">
+        <Instagram/>
+      </a>
+      <a href="https://github.com/bryancastillo10" target="_blank" rel="noopener noreferrer">
+        <Github/>
+      </a>
+      <a href="https://www.linkedin.com/in/bryan-castillo-479101171/" target="_blank" rel="noopener noreferrer">
+        <Linkedin/>
+      </a>
     </div>
   )
 }
