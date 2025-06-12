1f1d46dc12e71881d4cdc618c8747733e0855173
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-06-12 14:40:38 +0800

    🎨 assets: convert assets of social icons to components

---
+++ src/assets/socials/index.ts
@@ -0,0 +1,6 @@
+import Facebook from './Facebook';
+import Github from './Github';
+import Instagram from './Instagram';
+import LinkedIn from './LinkedIn';
+
+export { Facebook, Github, Instagram, LinkedIn };
--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -1,21 +1,29 @@
-import Image from 'next/image';
+import { Facebook, Instagram, Github, LinkedIn } from '@/assets/socials';
 
 const Footer = () => {
   const currentYr = new Date().getFullYear();
 
   return (
     <footer className="w-full xl:h-24 bg-accent-dark text-white flex overflow-hidden">
-      <section className="w-full max-w-5xl mx-auto flex flex-col md:flex-row justify-between p-4">
+      <section className="w-full max-w-5xl mx-auto flex flex-col items-center md:flex-row justify-between p-4">
         <div className="">
           <h1>&copy; {currentYr} Bryan's Portfolio </h1>
         </div>
         <div className="">
-          <Image
-            src="/socials/facebook.svg"
-            alt="fb-icon"
-            width={80}
-            height={80}
-          />
+          <ul className="flex flex-wrap gap-4">
+            <li>
+              <Facebook size={30} />
+            </li>
+            <li>
+              <Instagram size={30} />
+            </li>
+            <li>
+              <Github size={30} />
+            </li>
+            <li>
+              <LinkedIn size={30} />
+            </li>
+          </ul>
         </div>
       </section>
     </footer>
