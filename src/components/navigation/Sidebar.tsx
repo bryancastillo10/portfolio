2a3dbbec52ce05f35efcd367b843140db886fe0f
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-06-14 14:27:44 +0800

    🧩 component: sidebar item

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -1,10 +1,12 @@
 'use client';
-import { Plus, Minus } from 'lucide-react';
+import { ArrowRight, ArrowLeft } from 'lucide-react';
+import { User, CodeXml, Info, Wrench } from 'lucide-react';
 
 import { useAppSelector, useAppDispatch } from '@/lib/redux/hooks';
 import { setIsSidebarExpanded } from '@/lib/redux/slices/sidebarSlice';
 
 import Switch from '@/components/ui/Switch';
+import SidebarItem from '@/components/navigation/SidebarItem';
 
 const Sidebar = () => {
   const isSidebarExpanded = useAppSelector((state) => state.sidebar.isSidebarExpanded);
@@ -16,18 +18,26 @@ const Sidebar = () => {
 
   return (
     <aside
-      className={`hidden md:block fixed z-50 top-80 h-50 bg-accent-dark md:opacity-80 lg:opacity-100 text-background p-2 rounded-r-2xl shadow-md 
-		${isSidebarExpanded ? 'w-30' : 'w-12'} duration-500 ease-out`}
+      className={`hidden md:block fixed z-50 top-40 h-80 bg-accent-dark text-foreground md:opacity-80 lg:opacity-100  p-2 rounded-r-2xl shadow-md 
+		${isSidebarExpanded ? 'w-32' : 'w-12'} transition-all duration-500 ease-out`}
     >
       <div className={`flex flex-col gap-2 ${isSidebarExpanded ? "items-start": "items-center"}`}>
         <div
           onClick={toggleSidebar}
           className="cursor-pointer hover:bg-accent-light rounded-full w-fit p-1"
         >
-          {isSidebarExpanded ? <Minus /> : <Plus />}
+          {isSidebarExpanded ? <ArrowLeft /> : <ArrowRight />}
         </div>
+        <ul className="flex flex-col gap-2">
+            <SidebarItem name="Home" icon={User} />
+            <SidebarItem name="Projects" icon={CodeXml} />
+            <SidebarItem name="Tools" icon={Wrench} />
+            <SidebarItem name="About" icon={Info} />
+        </ul>
 
-        <Switch/>
+        <div className="my-2">
+          <Switch/>
+        </div>
       </div>
     </aside>
   );
---
+++ src/components/navigation/SidebarItem.tsx
@@ -0,0 +1,22 @@
+import { useAppSelector } from "@/lib/redux/hooks";
+import { LucideIcon } from "lucide-react";
+
+interface SidebarItemProps {
+	name: string;
+	icon: LucideIcon;
+};
+
+const SidebarItem = ({name, icon: Icon}: SidebarItemProps) => {
+  const isSidebarExpanded = useAppSelector((state) => state.sidebar.isSidebarExpanded);
+
+  return (
+	 <li className='flex items-center gap-2  p-2'>
+        <Icon /> 
+        <p className={`transition-all duration-500 ease-out overflow-hidden
+  ${isSidebarExpanded ? "max-w-[200px] opacity-100" : "max-w-0 opacity-0"}
+`}>{name}</p>
+    </li>
+  )
+}
+
+export default SidebarItem;
