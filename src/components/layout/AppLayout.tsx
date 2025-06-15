0f614dc73e43f745d8b2ddc93ab82f090e6266c0
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-06-15 21:49:03 +0800

    📱 layout: mobile menu sidebar

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -12,6 +12,8 @@ import Sidebar from '@/components/navigation/Sidebar';
 import { GithubProfileRes } from '@/interfaces/githubprofile';
 import { GithubRepoRes } from '@/interfaces/gitrepo';
 
+import MobileSidebar from '@/components/navigation/MobileSidebar';
+
 interface AppLayoutProps {
   profile: GithubProfileRes<string, number>;
   repos: GithubRepoRes<string>[];
@@ -21,6 +23,7 @@ const AppLayout = ({ profile, repos }: AppLayoutProps) => {
   return (
     <>
       <Sidebar />
+      <MobileSidebar />
       <section className="w-full max-w-5xl mx-auto flex flex-col items-center">
         <Hero profile={profile} />
         <GithubActivity />
