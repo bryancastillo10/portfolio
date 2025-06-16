ad4cd5d6bf79b25bd7f76c60d979f9f594905941
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-06-16 14:47:33 +0800

    🧩 component: Button

--- src/components/layout/ProjectDetailsLayout.tsx
+++ src/components/layout/ProjectDetailsLayout.tsx
@@ -1,19 +1,23 @@
 'use client';
 
 import { useAppSelector } from '@/lib/redux/hooks';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
+import Button from '@/components/ui/Button';
 import { ArrowLeft } from 'lucide-react';
 
 const ProjectDetailsLayout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
   const isDarkMode = useAppSelector(state => state.theme.isDarkMode);
 
   return (
     <section className={`${isDarkMode ? 'bg-gray' : 'bg-[#f4f3f2]'}`}>
       <div className="max-w-5xl mx-auto flex flex-col bg-background py-8 px-4">
-        <Link className="my-4" href="/">
-          <ArrowLeft size="20" />
-        </Link>
+        <div className="flex justify-between pb-8">
+          <Button variant="primary" action={() => router.push('/')}>
+            <ArrowLeft />
+          </Button>
+        </div>
         {children}
       </div>
     </section>
