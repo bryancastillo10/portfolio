3493286e4208578dfcd1a8e66d7b16516523380f
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-07-07 00:50:26 +0800

    🧹 chore: added github api revalidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Providers from '@/components/provider/Providers';
 import { getGithubData } from '@/api/github';
 import AppLayout from '@/components/layout/AppLayout';
 
+export const revalidate = 14400;
+
 export default async function Home() {
   const { profile, repos } = await getGithubData();
 
